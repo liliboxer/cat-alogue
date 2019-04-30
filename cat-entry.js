711b4936d76923bb7d69761b5b87a09c221e1e49
Lili Boxer <ninaliboxer@gmail.com>
2019-04-30 10:23:05 -0700

    age range slider number

--- cat-entry.js
+++ cat-entry.js
@@ -1,6 +1,13 @@
 import getEntry from './src/get-entry.js';
 
 const form = document.getElementById('cat-entry');
+const ageRange = document.getElementById('age');
+const ageDisplay = document.getElementById('age-display');
+
+ageRange.addEventListener('change', () => {
+    ageDisplay.textContent = ageRange.value;
+
+});
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
