6cdaa473f8e0133374c379fef65f32dcf1744b9f
Lili Boxer <ninaliboxer@gmail.com>
2019-04-30 09:07:35 -0700

    tdd test setup

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-import getEntry from "../src/get-entry.js";
+import getEntry from '../src/get-entry.js';
 
 const test = QUnit.test;
 
@@ -6,11 +6,35 @@ test('puts applicant form into object', function(assert) {
     //Arrange
     // Set up your parameters and expectations
 
+    const expected = {
+        owner: 'Lili',
+        name: 'Max',
+        age: 8,
+        type: 'calico',
+        color: ['ginger', 'white'],
+        hair: true,
+        indoor: true,
+        biscuits: true,
+        love: 'he is the fluffiest best!'
+    };
+
+    const formData = new FormData();
+    formData.set('owner', expected.owner);
+    formData.set('name', expected.name);
+    formData.set('age', '8');
+    formData.set('type', 'calico');
+    formData.set('color', expected.color[0]);
+    formData.append('color', expected.color[1]);
+    formData.set('hair', 'yes');
+    formData.set('indoor', 'yes');
+    formData.set('biscuits', 'yes');
+    formData.set('love', expected.love);
 
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const applicant = getEntry(formData);
 
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(expected, applicant);
 });
