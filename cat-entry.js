742e417163e137dee0482176e2b0cb7ceb94032c
Lili Boxer <ninaliboxer@gmail.com>
2019-04-30 10:39:42 -0700

    message upon submission

--- cat-entry.js
+++ cat-entry.js
@@ -3,6 +3,7 @@ import getEntry from './src/get-entry.js';
 const form = document.getElementById('cat-entry');
 const ageRange = document.getElementById('age');
 const ageDisplay = document.getElementById('age-display');
+const message = document.getElementById('message');
 
 ageRange.addEventListener('change', () => {
     ageDisplay.textContent = ageRange.value;
@@ -13,5 +14,6 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const applicant = getEntry(formData);
+    message.textContent = 'Your cat thanks for you for immortilizing it in catalogue form!';
     console.log(applicant);
 });
