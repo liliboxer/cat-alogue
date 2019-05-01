67b5f975f40e3c007f21773182f7826703d6a877
Lili Boxer <ninaliboxer@gmail.com>
2019-05-01 16:55:33 -0700

    refractored code to round trip for first index of array

--- src/cat-detail-api.js
+++ src/cat-detail-api.js
@@ -1,18 +1,21 @@
 const catDetailApi = {
     save(applicant) {
+        // create array with applicant
+        const cats = [];
+        cats.push(applicant);
         // serialize to JSON
-        const json = JSON.stringify(applicant);
+        const json = JSON.stringify(cats);
         // save to local storage
-        localStorage.setItem('applicant', json);
+        localStorage.setItem('cats', json);
     },
 
     get() {
         // get from local storage
-        const json = localStorage.getItem('applicant');
+        const json = localStorage.getItem('cats');
         // parse 
-        const applicant = JSON.parse(json);
+        const cats = JSON.parse(json);
         // return
-        return applicant;
+        return cats[0];
     }
 };
 
