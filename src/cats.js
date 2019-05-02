fe9c353d9d3d5d1e7417861787bb439cf174e8c1
Lili Boxer <ninaliboxer@gmail.com>
2019-05-02 13:00:17 -0700

    table being populated

--- src/cats.js
+++ src/cats.js
@@ -4,17 +4,50 @@ const tbody = document.getElementById('cats');
 
 const cats = catDetailApi.getAll();
 
-for (let i = 0; i < cats.length; i++) {
+for(let i = 0; i < cats.length; i++) {
     const cat = cats[i];
 
     const tr = document.createElement('tr');
+
     const ownerCell = document.createElement('td');
     ownerCell.textContent = cat.owner;
     tr.appendChild(ownerCell);
     
-    const tr = document.createElement('tr');
-    const nameCell = document.createElement('tr');
-    nameCell.textContent = cat.owner;
+    const nameCell = document.createElement('td');
+    nameCell.textContent = cat.name;
+    tr.appendChild(nameCell);
+
+    const ageCell = document.createElement('td');
+    ageCell.value = cat.age;
+    tr.appendChild(ageCell);
+    
+    const typeCell = document.createElement('td');
+    typeCell.textContent = cat.type;
+    tr.appendChild(typeCell);
+
+    const colorCell = document.createElement('td');
+    let colorList = '';
+    if(cat.color) {
+        colorList = cat.color.join(', ');
+    }
+    colorCell.textContent = colorList;
+    tr.appendChild(colorCell);
+    console.log('cat color', cat.color);
+
+    const hairCell = document.createElement('td');
+    hairCell.textContent = cat.hair;
+    tr.appendChild(hairCell);
+
+    const indoorCell = document.createElement('td');
+    indoorCell.textContent = cat.indoor;
+    tr.appendChild(indoorCell);
+
+    const biscuitsCell = document.createElement('td');
+    biscuitsCell.textContent = cat.indoor;
+    tr.appendChild(biscuitsCell);
+
+    tbody.appendChild(tr);
+
 }
 
 
