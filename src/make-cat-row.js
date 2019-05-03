6f14c518f6c68a756dc04bbbfface4dfa72110d9
Lili Boxer <ninaliboxer@gmail.com>
2019-05-03 16:33:27 -0700

    re ordered makeCatRow

--- src/make-cat-row.js
+++ src/make-cat-row.js
@@ -1,28 +1,21 @@
 function makeCatRow(cat) {
     const tr = document.createElement('tr');
-
     const ownerCell = makeOwnerCell(cat.owner);
-    tr.appendChild(ownerCell);
-
     const nameCell = makeTextCell(cat.name);
-    tr.appendChild(nameCell);
-    
     const ageCell = makeTextCell(cat.age);
-    tr.appendChild(ageCell);
-    
     const typeCell = makeTextCell(cat.type);
-    tr.appendChild(typeCell);
-
     const colorCell = makeColorCell(cat.color);
-    tr.appendChild(colorCell);
-
     const hairCell = makeTextCell(cat.hair);
-    tr.appendChild(hairCell);
-
     const indoorCell = makeTextCell(cat.indoor);
-    tr.appendChild(indoorCell);
-
     const biscuitsCell = makeTextCell(cat.biscuits);
+
+    tr.appendChild(ownerCell);
+    tr.appendChild(nameCell);
+    tr.appendChild(ageCell);
+    tr.appendChild(typeCell);
+    tr.appendChild(colorCell);
+    tr.appendChild(hairCell);
+    tr.appendChild(indoorCell);
     tr.appendChild(biscuitsCell);
 
     return tr;
